1e117ebc598e8a2d9b60c6f2d3de992849504ebd
Brad Pyne <bradpyne@quickenloans.com>
2014-03-31 15:51:01 -0400

    Set up IoC

--- RiskMeter.Core/App.cs
+++ RiskMeter.Core/App.cs
@@ -1,17 +1,16 @@
-using Cirrious.CrossCore.IoC;
+using Cirrious.CrossCore;
+using Cirrious.MvvmCross.ViewModels;
+using RiskMeter.Core.Services;
+using RiskMeter.Core.ViewModels;
 
 namespace RiskMeter.Core
 {
-    public class App : Cirrious.MvvmCross.ViewModels.MvxApplication
+    public class App : MvxApplication
     {
         public override void Initialize()
         {
-            CreatableTypes()
-                .EndingWith("Service")
-                .AsInterfaces()
-                .RegisterAsLazySingleton();
-				
-            RegisterAppStart<ViewModels.RiskViewModel>();
+            Mvx.RegisterType<IRiskCalculator, RiskCalculator>();
+            Mvx.RegisterSingleton<IMvxAppStart>(new MvxAppStart<RiskViewModel>());
         }
     }
 }
