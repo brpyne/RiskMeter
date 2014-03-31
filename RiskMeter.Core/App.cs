a61785f80a35ef2aefaece4e81b159a84f8b2dd3
Brad Pyne <bradpyne@quickenloans.com>
2014-03-31 15:47:39 -0400

    Replaced templated FirstViewModel with RiskViewModel

--- RiskMeter.Core/App.cs
+++ RiskMeter.Core/App.cs
@@ -11,7 +11,7 @@ namespace RiskMeter.Core
                 .AsInterfaces()
                 .RegisterAsLazySingleton();
 				
-            RegisterAppStart<ViewModels.FirstViewModel>();
+            RegisterAppStart<ViewModels.RiskViewModel>();
         }
     }
 }
