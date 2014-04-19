5c854637bf94ed91b963e16a7d05630a116129af
Pyne, Brad <BradPyne@quickenloans.com>
2014-04-18 20:57:03 -0400

    Referenced Locally stored files in pseudocode for web scraper

--- RiskMeter.WebScraper/Program.cs
+++ RiskMeter.WebScraper/Program.cs
@@ -7,12 +7,15 @@ namespace RiskMeter.WebScraper
         static void Main(string[] args)
         {
             // 1. Retrieve listing of state directories
-            // http://www.city-data.com/crime/
+            // Source: http://www.city-data.com/crime/
+            // /data/state-directory.htm
 
             // 2. Iterate through each state directory and retrieve listing of their respective city directory
-            // http://www.city-data.com/crime/crime-Alabama.html
+            // Source http://www.city-data.com/crime/crime-Michigan.html
+            // /data/michigan-directory.htm
             
             // 3. Retrieve crime data from each city for storage
+            // /data/detroit-data.htm
             var scraper = new CrimeDataScraper("/Data/detroit-data.htm");
             scraper.GetData();
 
