5d844e9c156391de7e69c42b47a49aea47a4992c
Pyne, Brad <BradPyne@quickenloans.com>
2014-04-18 20:51:44 -0400

    Pseudocode for WebScraper

--- RiskMeter.WebScraper/Program.cs
+++ RiskMeter.WebScraper/Program.cs
@@ -1,16 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RiskMeter.WebScraper;
 
-namespace WebScrape
+namespace RiskMeter.WebScraper
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // 1. Retrieve listing of state directories
+            // http://www.city-data.com/crime/
+
+            // 2. Iterate through each state directory and retrieve listing of their respective city directory
+            // http://www.city-data.com/crime/crime-Alabama.html
+            
+            // 3. Retrieve crime data from each city for storage
             var scraper = new CrimeDataScraper("/Data/detroit-data.htm");
             scraper.GetData();
 
